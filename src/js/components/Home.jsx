c3ce7a2c82e47770c4a8b2d0426cb18e2d4f1683
JBeanCode <js02bean@gmail.com>
2026-02-19 02:07:32 +0000

    joshs-final-simpleToDo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,40 +3,68 @@ import { Todo } from "./Todo";
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("")
-	const [todos, setTodos] = useState([])
-	
-	const handleInputChange = (event) => {
-		if(event.key === "Enter") {
-			addTodo();
-		}
-	}
-	const addTodo = () => {
-		if(inputValue.length > 0){
-			setTodos([...todos, inputValue])
-			setInputValue("")
-		}
-	}
-	return (
-		<div className="w-50 m-auto">
-			<h1 className="text-center mt-5">To Do List!</h1>
-			<div className="d-flex justify-content-center">
-				<input type = "text" className="me-2 rounded-3" value = {inputValue} onChange={(event) => setInputValue(event.target.value)} onKeyDown={handleInputChange}/>
-				<button type="button" className="btn btn-primary" onClick={() => addTodo()} >Add Task</button>
-			</div>
-			<div className="">
-			{todos.map((todoValue,index) => (
-					<Todo todoValue={todoValue} setTodos = {setTodos} index={index} key={index} todos={todos}/>
-				))}
-			</div>
-			<div className={`bg-primary-subtle row py-2 rounded-3 ${todos.length < 1 ? "bg-success-subtle" : ""}`}>
-				<p className="text-end mb-0">{ todos.length === 1 ? `${todos.length} item left!` : `${todos.length} items left!` }</p>
-			</div>
-			<div className="mt-5 text-center">
-				<p>Made by <strong className="text-danger">love</strong> by JBeanCode</p>
-			</div>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleInputChange = (event) => {
+    if (event.key === "Enter") {
+      addTodo();
+    }
+  };
+  const addTodo = () => {
+    if (inputValue.length > 0) {
+      setTodos([...todos, {inputValue, isPinned: false}]);
+      setInputValue("");
+    }
+  };
+  return (
+    <div className="w-50 m-auto">
+      <h1 className="text-center mt-5">To Do List!</h1>
+      <div className="d-flex justify-content-center">
+        <input
+          type="text"
+          className="me-2 rounded-3"
+          value={inputValue}
+		  //todos.
+          onChange={(event) => setInputValue(event.target.value)}
+          onKeyDown={handleInputChange}
+        />
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => addTodo()}
+        >
+          Add Task
+        </button>
+      </div>
+      <div className="">
+        {todos.map((todo, index) => (
+          <Todo
+            todoValue={todo.inputValue}
+            setTodos={setTodos}
+            index={index}
+            key={index}
+            todos={todos}
+			isPinned={todo.isPinned}
+          />
+        ))}
+      </div>
+      <div
+        className={`row py-2 rounded-3 ${todos.length < 1 ? "bg-success-subtle" : "bg-primary-subtle"}`}
+      >
+        <p className="text-end mb-0">
+          {todos.length === 1
+            ? `${todos.length} item left!`
+            : `${todos.length} items left!`}
+        </p>
+      </div>
+      <div className="mt-5 text-center">
+        <p>
+          Made by <strong className="text-danger">love</strong> by JBeanCode
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/components/Todo.jsx
+++ src/js/components/Todo.jsx
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
-export const Todo = ({ todoValue, setTodos, todos, index }) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbtack } from "@fortawesome/free-solid-svg-icons";
+
+export const Todo = ({ todoValue, setTodos, todos, index, isPinned }) => {
   // add useState with "onHover" - as a terniary to text if user is hovering and if so, then use the button tag in line 20. Potentially
   // add functionality for edit ability for clicking within the text or div.
   const [divHover, setDivHover] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [editValue, setEditValue] = useState(todoValue);
+//  const [isPinned, setIsPinned] = useState(false);
 
   const deleteTodos = () => {
     console.log("todo to be deleted: ", index);
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
+    if (!isPinned) {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+      return;
+    } window.alert("Cannot delete a pinned task!")
   };
+
   const editTodos = () => {
     const newTodos = [...todos];
     newTodos[index] = editValue;
     setTodos(newTodos);
     setIsEditing(false);
   };
+  const invertPinned = () => {
+    const newTodos = [...todos];
+    newTodos[index].isPinned = !isPinned;
+    setTodos(newTodos);
+  }
 
   return (
     <div
@@ -25,9 +38,27 @@ export const Todo = ({ todoValue, setTodos, todos, index }) => {
       onMouseEnter={() => setDivHover(true)}
       onMouseLeave={() => setDivHover(false)}
     >
-      <div className="col-9">
+      <div className={"col-1 d-flex rounded-3 align-items-center "}>
+        {divHover || isPinned ? (
+          <button
+            type="button"
+            className={"btn btn-outline-warning me-auto  "+ (isPinned ? " btn-warning" : "")}
+            onClick={() => invertPinned()}
+          >
+            <FontAwesomeIcon icon={faThumbtack} className="text-dark" />
+          </button>
+        ) : null}
+      </div>
+      <div className="col-6">
         {/* If editing → show input */}
-        {isEditing ? (<input type="text" className="form-control" value={editValue} autoFocus onChange={(e) => setEditValue(e.target.value)} onBlur={editTodos}
+        {isEditing ? (
+          <input
+            type="text"
+            className="form-control"
+            value={editValue}
+            autoFocus
+            onChange={(e) => setEditValue(e.target.value)}
+            onBlur={editTodos}
             onKeyDown={(e) => {
               if (e.key === "Enter") editTodos();
               if (e.key === "Escape") setIsEditing(false);
@@ -41,7 +72,15 @@ export const Todo = ({ todoValue, setTodos, todos, index }) => {
         )}
       </div>
       <div className="col-3 d-flex rounded-3 align-items-center ">
-        {divHover && (<button type="button" className="btn btn-danger ms-auto" onClick={deleteTodos}>X</button>)}
+        {divHover && (
+          <button
+            type="button"
+            className="btn btn-danger ms-auto"
+            onClick={deleteTodos}
+          >
+            X
+          </button>
+        )}
       </div>
     </div>
   );
